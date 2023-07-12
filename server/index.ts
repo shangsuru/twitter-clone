c9c9aa04e44e3e5ababf38e726dfb8954f71df1c
shangsuru <helm.henry99@gmail.com>
2023-07-12 13:54:23 +0900

    Add Express

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+
+    server.get("/api/todolist", (req: Request, res: Response) => {
+      return res.send({ list: [] });
+    });
+
+    server.all("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();

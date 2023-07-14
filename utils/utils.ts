450f8d3c94e3903c55bf71fdd974bb9eae238041
shangsuru <helm.henry99@gmail.com>
2023-07-14 17:45:18 +0900

    Add function that computes a X ago string

---
+++ utils/utils.ts
@@ -0,0 +1,34 @@
+type UnixTime = number;
+
+export function timeAgo(ts: UnixTime): string {
+  // This function computes the delta between the
+  // provided timestamp and the current time, then test
+  // the delta for predefined ranges.
+
+  var now = Math.floor(Date.now() / 1000);
+  var seconds = now - ts;
+
+  const minutes = 60;
+  const hour = minutes * 60;
+  const day = hour * 24;
+  const month = day * 30;
+
+  if (seconds / month >= 1) {
+    let date = new Date(ts * 1000);
+    return `${date.getMonth()}/${date.getDay()}/${date.getFullYear()}`;
+  }
+
+  if (seconds / day >= 1) {
+    return `${Math.floor(seconds / day)}d`;
+  }
+
+  if (seconds / hour >= 1) {
+    return `${Math.floor(seconds / hour)}h`;
+  }
+
+  if (seconds / minutes >= 1) {
+    return `${Math.floor(seconds / minutes)}m`;
+  }
+
+  return "1m";
+}

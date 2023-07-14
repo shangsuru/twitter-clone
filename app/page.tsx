fa6b9a62f3d52332db54a00fab25edda08a528f0
shangsuru <helm.henry99@gmail.com>
2023-07-14 17:45:55 +0900

    Add created_at to tweet data

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ type TweetData = {
   sender: string;
   handle: string;
   text: string;
+  created_at: number;
 };
 
 const all: TweetData[] = [
@@ -17,26 +18,31 @@ const all: TweetData[] = [
     sender: "Gynvael Coldwind",
     handle: "@gynvael",
     text: "Friday was my last day at Google. I'm saddened to leave behind my team of last 12+ years, but I know Google Security is in great hands.  It's time for a short break and then I'm moving on with plans I've made long ago - my own sec research, consulting, and education company.",
+    created_at: 1689317981,
   },
   {
     sender: "John Hammond",
     handle: "@_JohnHammond",
     text: "For another fireworks show, Ignacio Dominguez and Carlos Polop from HALBORN showcase how dependency confusion attacks can occur with the AWS Code Artifact service -- potentially even having npm execute rogue code just upon install!",
+    created_at: 1689315000,
   },
   {
     sender: "Yaron (Ron) Minsky",
     handle: "@yminsky",
     text: "So...does anyone have advice for picking between the various and sundry Python type systems? mypy, pyright, pyre, pytype...how do you pick?",
+    created_at: 1689299980,
   },
   {
     sender: "DevSecCon",
     handle: "@devseccon",
     text: "Missed the interactive workshop on OWASP TOP 10 Security API 2023 x GraphQL at DSC24? Watch now...",
+    created_at: 1681447919,
   },
   {
     sender: "Clint Gibler",
     handle: "@clintgibler",
     text: "findmytakeover scans aws, azure, and google cloud for dangling DNS records and potential subdomain takeovers #cloudsec",
+    created_at: 1586839919,
   },
 ];
 
@@ -45,16 +51,19 @@ const following: TweetData[] = [
     sender: "Yaron (Ron) Minsky",
     handle: "@yminsky",
     text: "So...does anyone have advice for picking between the various and sundry Python type systems? mypy, pyright, pyre, pytype...how do you pick?",
+    created_at: 1689299980,
   },
   {
     sender: "DevSecCon",
     handle: "@devseccon",
     text: "Missed the interactive workshop on OWASP TOP 10 Security API 2023 x GraphQL at DSC24? Watch now...",
+    created_at: 1681447919,
   },
   {
     sender: "Clint Gibler",
     handle: "@clintgibler",
     text: "findmytakeover scans aws, azure, and google cloud for dangling DNS records and potential subdomain takeovers #cloudsec",
+    created_at: 1586839919,
   },
 ];
 
@@ -72,6 +81,7 @@ const items: TabsProps["items"] = [
         sender={tweet.sender}
         handle={tweet.handle}
         text={tweet.text}
+        created_at={tweet.created_at}
       />
     )),
   },
@@ -84,6 +94,7 @@ const items: TabsProps["items"] = [
         sender={tweet.sender}
         handle={tweet.handle}
         text={tweet.text}
+        created_at={tweet.created_at}
       />
     )),
   },

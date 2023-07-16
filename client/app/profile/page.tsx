13459b6d3645e006419299011ca1ead752bf3c3d
shangsuru <helm.henry99@gmail.com>
2023-07-16 16:40:50 +0900

    Move code into EditProfileModal

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import React, { useState } from "react";
 import Link from "next/link";
-import { Avatar, Button, Divider, Typography, Image, Modal, Input } from "antd";
+import { Divider, Typography, Image } from "antd";
 import {
   EnvironmentOutlined,
   LinkOutlined,
@@ -9,6 +8,7 @@ import {
 } from "@ant-design/icons";
 
 import TweetCard from "@/components/TweetCard";
+import EditProfileModal from "@/components/EditProfileModal";
 import { timeAgo } from "@/utils/utils";
 
 const { Title, Text } = Typography;
@@ -32,29 +32,6 @@ type TweetData = {
 };
 
 export default function Profile() {
-  const [loading, setLoading] = useState(false);
-  const [open, setOpen] = useState(false);
-
-  function showModal() {
-    setOpen(true);
-  }
-
-  function handleModalOk() {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      setOpen(false);
-    }, 3000);
-  }
-
-  function handleModalCancel() {
-    setOpen(false);
-  }
-
-  function onTextAreaChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-    console.log("Change:", e.target.value);
-  }
-
   const user: UserData = {
     username: "John Hammond",
     handle: "@_JohnHammond",
@@ -128,63 +105,7 @@ export default function Profile() {
             src="https://yt3.googleusercontent.com/6FqcWoHZvrZixaGi1S3Re3Z90SCS3iq2_36hQSnSHQPtQVVkywH8WKka53MiBYBSP6DmqM-g9w=s900-c-k-c0x00ffffff-no-rj"
           />
           <br />
-          <Button style={{ marginTop: 10 }} shape="round" onClick={showModal}>
-            Edit Profile
-          </Button>
-          <Modal
-            open={open}
-            title={
-              <div>
-                <Avatar
-                  src="https://yt3.googleusercontent.com/6FqcWoHZvrZixaGi1S3Re3Z90SCS3iq2_36hQSnSHQPtQVVkywH8WKka53MiBYBSP6DmqM-g9w=s900-c-k-c0x00ffffff-no-rj"
-                  className="avatar"
-                />
-
-                <span>Edit Profile</span>
-              </div>
-            }
-            onOk={handleModalOk}
-            onCancel={handleModalCancel}
-            footer={[
-              <Button
-                key="submit"
-                type="primary"
-                loading={loading}
-                onClick={handleModalOk}
-              >
-                Save
-              </Button>,
-            ]}
-          >
-            <Divider />
-            <Input
-              className="simple-label"
-              addonBefore="Name"
-              placeholder="John Hammond"
-              bordered={false}
-            />
-            <Divider />
-            <Input
-              className="simple-label"
-              addonBefore="Bio"
-              placeholder="Add a bio to your profile"
-              bordered={false}
-            />
-            <Divider />
-            <Input
-              className="simple-label"
-              addonBefore="Location"
-              placeholder="Add your location"
-              bordered={false}
-            />
-            <Divider />
-            <Input
-              className="simple-label"
-              addonBefore="Website"
-              placeholder="Add your website"
-              bordered={false}
-            />
-          </Modal>
+          <EditProfileModal />
         </div>
       </div>
       <Divider />

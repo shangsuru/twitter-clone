b5fa5e39a9b351705784e8ca79194d3915a40f54
shangsuru <helm.henry99@gmail.com>
2023-07-19 14:45:48 +0900

    Add dummy profile data

--- client/app/profile/[userId]/page.tsx
+++ client/app/profile/[userId]/page.tsx
@@ -78,6 +78,7 @@ export default function Profile({ params }: { params: { userId: string } }) {
   }
 
   const ownHandle = data?.user?.email!.split("@")[0];
+  const ownImage = data?.user?.image ?? "/user_icon.png";
 
   fetch(`http://localhost:4000/users/profile/${params.userId}`).then((res) => {
     if (res.ok) {
@@ -95,7 +96,7 @@ export default function Profile({ params }: { params: { userId: string } }) {
 
   return (
     <AntdStyle>
-      <Header handle={ownHandle!} image={image} />
+      <Header handle={ownHandle!} image={ownImage} />
       <div id="profile-header">
         <div>
           <Title level={3} style={{ marginBottom: 2 }}>
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import routes from "./routes/routes";
 import dynamoose from "dynamoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import addDummyData from "./utils/initDB";
 
 const app: Express = express();
 const port = 4000;
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
 dynamoose.aws.ddb.local("http://localhost:8000");
+
+addDummyData();
 
 routes(app);
 

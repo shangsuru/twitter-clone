1b811d93c69436324f54e56ca0d865fd9fb631b7
shangsuru <helm.henry99@gmail.com>
2023-08-02 18:18:11 +0900

    Add test workflow

---
+++ run-tests.sh
@@ -0,0 +1,4 @@
+cd server
+npm install
+NODE_ENV=development npm run build
+NODE_ENV=development npm test
--- server/src/controllers/TweetController.ts
+++ server/src/controllers/TweetController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import {
   deleteImage,
   imageKeysToPresignedUrl,
